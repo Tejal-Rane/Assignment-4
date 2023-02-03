522466a07bd3af66e4551bac834154e82c1cdc9c
Tejal-Rane <87368902+Tejal-Rane@users.noreply.github.com>
2023-02-04 01:10:03 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import App from './App';
-import chooseplayer from "choosePlayer"
-
-ReactDOM.render(<App />, document.getElementById('root'));
